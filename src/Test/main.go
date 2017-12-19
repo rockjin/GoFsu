e7ce91387658d99ac5949acf037a5e49961ea113
rock_jin <rock_jin@126.com>
2017-12-19 21:28:08 +0800

    go c 测试。

--- src/Test/main.go
+++ src/Test/main.go
@@ -1,22 +1,12 @@
 package main
 
+/*
+#include "test.h"
+*/
 import (
-	"bufio"
-	"fmt"
-	"strings"
-	"time"
+	"C"
 )
 
 func main() {
-	reader := bufio.NewReaderSize(strings.NewReader("http://studygolang.com.\t It is the home of gophers"), 14)
-	go Peek(reader)
-	go reader.ReadBytes('\t')
-	time.Sleep(1e8)
-}
-
-func Peek(reader *bufio.Reader) {
-	line, _ := reader.Peek(14)
-	fmt.Printf("%s\n", line)
-	time.Sleep(1)
-	fmt.Printf("%s\n", line)
+	C.func()
 }
